63ea4ee0e4cb6b7a4445d81b62b6f374ab8bc07d
Shouma63 <choma63.twins24@gmail.com>
2020-09-26 16:12:49 +0900

    initial commit

---
+++ app/assets/javascripts/modules/posts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".typed").typed({
+    strings: ["Welcome to Musclebot.", "Let's Best Training."],
+    typeSpeed: 100,
+    startDelay: 1000,
+    backSpeed: 20,
+    loop: true,
+    showCursor: false,
+    backDelay: 500
+  });
+});
