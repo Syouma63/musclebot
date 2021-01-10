3c0a916e0a314479159bda7b18b1398a6f62fc30
Shouma63 <choma63.twins24@gmail.com>
2021-01-10 12:07:28 +0900

    devise-image-preview

--- app/assets/javascripts/modules/users.js
+++ app/assets/javascripts/modules/users.js
@@ -1,3 +1,55 @@
+$(document).ready(function () {
+  var view_box = $('.img-user');
+   
+  $(".fa-image").on('change', function(){
+     var fileprop = $(this).prop('files')[0],
+         find_img = $(this).next('img'),
+         fileRdr = new FileReader();
+     
+     if(find_img.length){
+        find_img.nextAll().remove();
+        find_img.remove();
+     }
+     
+    var img = '<img class="img_view select mypage__container__header__picture__image"><br>'
+    // var del = '<div class="img_del">画像を削除する</div>'
+
+    view_box.append(img);
+    // view_box.append(del);
+     
+    fileRdr.onload = function() {    
+      view_box.find('img').attr('src', fileRdr.result);
+      // img_del(view_box); 
+    }
+    fileRdr.readAsDataURL(fileprop);  
+  });
+
+  // function img_del(target)
+  // {
+  //    target.find(".img_del").on('click',function(){
+ 
+  //     if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
+  //     {
+  //        $(this).parent().find('input[type=file]').val('');
+  //        $(this).parent().find('.img_view, br').remove();
+  //        $(this).remove();
+  //     }
+ 
+  //     return false;
+  //   });
+  // }  
+});
+
+
+// 編集ページ 動画を変更するを押したら動画が消える
+$(function() {
+  $('.images').click(function() {
+    $('.select').fadeOut();
+    $('.img_del').fadeOut();
+  });
+});
+
+
 // クリックしてたら表示させる記述
 // $(function() {
 //   $('.image-image').click(function() {
