e708c6737178e89568825f989faabaa4be37cedc
Shouma63 <choma63.twins24@gmail.com>
2021-01-12 22:34:40 +0900

    tweet-preview-fix

--- app/assets/javascripts/modules/tweets.js
+++ app/assets/javascripts/modules/tweets.js
@@ -1,50 +1,34 @@
-$(document).ready(function () {
-  var view_box = $('.img-img');
-   
-  $(".img-video").on('change', function(){
-     var fileprop = $(this).prop('files')[0],
-         find_img = $(this).next('video'),
-         fileRdr = new FileReader();
-     
-     if(find_img.length){
-        find_img.nextAll().remove();
-        find_img.remove();
-     }
-     
-    var video = '<video width="300" class="img_view select" loop controls muted><br>'
-    // var del = '<div class="img_del">画像を削除する</div>'
+$(document).on('turbolinks:load', function() {
+  $fileField = $('#upload-image')
 
-    view_box.append(video);
-    // view_box.append(del);
-     
-    fileRdr.onload = function() {    
-      view_box.find('video').attr('src', fileRdr.result);
-      // img_del(view_box); 
-    }
-    fileRdr.readAsDataURL(fileprop);  
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $(".img-img");
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<video>').attr({
+          src: e.target.result,
+          width: "300px",
+          class: "select img-preview",
+          controls: "true",
+          loop: "loop",
+          playsinline: "true",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
-
-  // function img_del(target)
-  // {
-  //    target.find(".img_del").on('click',function(){
- 
-  //     if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
-  //     {
-  //        $(this).parent().find('input[type=file]').val('');
-  //        $(this).parent().find('.img_view, br').remove();
-  //        $(this).remove();
-  //     }
- 
-  //     return false;
-  //   });
-  // }  
 });
 
 
 // 編集ページ 動画を変更するを押したら動画が消える
 $(document).on('turbolinks:load',function() {
   $('.img-sel').click(function() {
-    $('.select').fadeOut();
+    $('.img-preview').fadeOut();
     $('.img_del').fadeOut();
   });
 });
