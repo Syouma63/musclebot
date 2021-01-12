67f0cd82371149bff591e8d9d45a4c5e534cc5a6
Shouma63 <choma63.twins24@gmail.com>
2021-01-12 20:22:40 +0900

    comment.ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      render tweet_path(@comment.tweet.id)
+    end
   end
 
   private
